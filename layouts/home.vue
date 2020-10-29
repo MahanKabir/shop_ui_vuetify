91dbb4111b165bf1f92c0378557455547eb5f3c3
Mahan <mahan.kabir@gmail.com>
2020-10-30 02:35:43 +0330

    version_1

---
+++ layouts/home.vue
@@ -0,0 +1,34 @@
+<template>
+<v-app>
+
+    <nuxt/>
+    <f></f>
+
+</v-app>
+
+</template>
+
+
+<script>
+import SlideShow from '~/components/slideshow.vue'
+import Header from '~/components/header.vue'
+import Footer from '~/components/footer.vue'
+import CardPage from '~/components/Product/CardPage.vue'
+import TabPage from '~/components/Product/TabPage'
+import ModalPage from '~/components/Product/ModalPage'
+import CarouselsPage from '~/components/Product/CarouselsPage'
+
+
+export default {
+    components:{
+        slideshow: SlideShow,
+        h: Header,
+        f: Footer,
+        CardPage: CardPage,
+        TabPage:TabPage,
+        ModalPage: ModalPage,
+        CarouselsPage: CarouselsPage,
+    }
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <slideshow></slideshow>
+    <v-container>
+    <v-card>
+        <v-layout wrap align-content-space-around>
+        <v-flex><card></card></v-flex>
+        <v-flex><card></card></v-flex>
+        <v-flex><card></card></v-flex>
+        <v-flex><card></card></v-flex>
+        <v-flex><card></card></v-flex>
+        <v-flex><card></card></v-flex>
+        </v-layout>
+    </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  layout: 'home',
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
